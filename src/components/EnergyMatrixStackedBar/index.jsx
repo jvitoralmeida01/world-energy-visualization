ac645b4dfd4dd927d457cef1c123e15de2919407
Renatto Padilha <rspf@cin.ufpe.br>
2022-05-05 09:52:58 -0300

    Adding EnergyMatrixStackedBar Component
    Adding stackedBar component

---
+++ src/components/EnergyMatrixStackedBar/index.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { Bar } from 'react-chartjs-2';
+
+export default function EnergyMatrixStackedBar({dataset, labels}){
+  let data = {
+    labels: labels,
+    datasets: [
+    {
+      label: "Biofuel",
+      data: [dataset[0]],
+      backgroundColor: 'Green',
+      stack: 'Stack 0'
+    },
+    {
+      label: "Coal",
+      data: [dataset[1]],
+      backgroundColor: 'Red',
+      stack: 'Stack 0'
+    },
+    {
+      label: "Gas",
+      data: [dataset[2]],
+      backgroundColor: 'Orange',
+      stack: 'Stack 0'
+    },
+    {
+      label: "Hydro",
+      data: [dataset[3]],
+      backgroundColor: 'Cyan',
+      stack: 'Stack 0'
+    },
+    {
+      label: "Nuclear",
+      data: [dataset[4]],
+      backgroundColor: 'Purple',
+      stack: 'Stack 0'
+    },
+    {
+      label: "Oil",
+      data: [dataset[5]],
+      backgroundColor:'Black',
+      stack: 'Stack 0'
+    },
+    {
+      label: "Solar",
+      data: [dataset[6]],
+      backgroundColor:'Yellow',
+      stack: 'Stack 0'
+    },
+    {
+      label: "Wind",
+      data: [dataset[7]],
+      backgroundColor: 'Pink',
+      stack: 'Stack 0'
+    }
+  ],
+  };
+  
+  const options = {
+    indexAxis: 'y',
+    responsive: true,
+    plugins: {
+      title: {
+        color: '#fff',
+        display: true,
+        text: 'Country Energy Matrix',
+      },
+    },
+    scales:{
+      y:{
+        stacked: true,
+      },
+    }
+  };
+
+  return (
+    <div>
+      <Bar data={data} options={options} />
+    </div>);
+}
